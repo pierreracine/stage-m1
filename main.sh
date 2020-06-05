cc2f0b1cd185706ea9b872efa0c6602ce3ffea0c
pierreracine <pierreracine.pro@gmail.com>
2020-06-05 16:41:40 +0200

    adding new main script

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#SBATCH -p xeonv1_mono -c 1 -n 1 -N 1
+#SBATCH -p xeonv1_mono -c 1 -n 8 -N 1
 
 
 #Welcome to main.sh. This script uses all the other .py scripts and runs the calculations.
@@ -28,30 +28,34 @@ module load python/python-2.7.15-gcc-8.2.0
 module load python/python-3.7.0-gcc-4.8.5
 module load python/python-3.7.0-gcc-8.2.0
 
-cat informations | while read line;
-do
+cat informations | while read line;do
+
         read -ra ADDR <<< "$line"
-        a1==${ADDR[0]}
+        a1=${ADDR[0]}
         a2=${ADDR[1]}
         b=${ADDR[2]}
         f=${ADDR[3]}
-
-	python3 inp.py $a1 $a2  $b $f              #execution of the script writing the input files for Gaussian
+	dist_start=${ADDR[4]}
+	gap=${ADDR[5]}
+	step=${ADDR[6]}
+	python3 inp.py $a1 $a2  $b $f $dist_start $gap $step      #execution of the script writing the input files for Gaussian
 
 	g16 optGS_DFT.inp
 	g16 scan_DFT_s.inp				#SCan for the ground state
 	g16 scan_DFT_t.inp 
+	g16 SP_DFT_s.inp
+	g16 SP_DFT_t.inp
 
-	for k in `seq 1 3`;				#OPtimization for excited states
+	for k in `seq 1 5`;				#OPtimization for excited states
 	do
 		g16 optES_TD_s$k.inp
-	        g16 optES_TD_t$k.inp
+		g16 optES_TD_t$k.inp
 	done
 
-	g16 vert_trans_TD.inp				#Vertical transition calculations
+	python3 errors.py			          #script to write the file listing all errors
+	python3 exploitlog.py $dist_start $gap $step                             #script to extract energies and optimal geometries
+	python3 graph.py $a1 $a2  $b $f $dist_start $gap $step      #sript to plot the graphs of energies for 2N+1 states
+#	python3 store.py $a1 $a2  $b $f                   #script to rename and store all files
+#	rm *.inp Gau* scan_* opt* *.chk en* sym*
 
-	python3 exploitlog.py                      #script to extract energies and optimal geometries
-	python3 errors.py			#script to write the file listing all errors
-	python3 graph.py $a1 $a2  $b $f                           #sript to plot the graphs of energies for 2N+1 states
-	python3 store.py $a1 $a2  $b $f           #script to rename and store all files
 done
